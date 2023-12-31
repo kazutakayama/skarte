eda946d94a41346202eb0bb4b930774f5a6f78d7
kazutaka <k89z10@yahoo.co.jp>
2023-12-31 10:07:52 +0900

    skarte

---
+++ src/main/java/com/example/skarte/SkarteApplication.java
@@ -0,0 +1,13 @@
+package com.example.skarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkarteApplication.class, args);
+	}
+
+}
